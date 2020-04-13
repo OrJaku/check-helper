62b946ecac62d432573c02782216c40c40cb2c21
OrJaku <kuba.orzel@hotmail.com>
2020-04-13 20:47:39 +0200

    Developing application notes

--- notes/views.py
+++ notes/views.py
@@ -16,9 +16,15 @@ def notes_list(request, *args, **kwargs):
             )
         new_note.save()
         return redirect("/notes_list/")
+    date = Notes.objects.all().values('date').distinct().order_by('-date')
+    days = []
+    for i in date:
+        days.append(i['date'])
 
     notes = Notes.objects.all().filter(user=request.user)
+
     context = {
+        "days": days,
         "notes": notes,
     }
     return render(request, 'notes_list.html', context)
